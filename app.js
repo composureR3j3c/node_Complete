72b41900e607ccba705e187b3f238d40ee55e2e8
bereket <gezahegnebereket@gmail.com>
2023-09-30 11:56:31 +0300

    stream

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const events = require('events');
+const fs = require("fs");
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
 
-let myEmitter = new events.EventEmitter()
+const app = express();
+app.use(bodyParser.json());
 
-myEmitter.on('userCreated',()=>{
-    console.log("User Created via emmiter !")
-})
+app.listen(process.env.PORT || 7000, async () => {
+  console.log("listening on port", 7000 || process.env.PORT);
+});
 
-myEmitter.on('userCreated',()=>{
-    console.log("User Created added to the database via emmiter !")
-})
+app.get("/", (req, res) => {
+  // Badway
+  // fs.readFilestre("./files/bigfile.txt", (err, data) => {
+  //   if(err){
+  //       console.log
+  //       res.end('sth went wrong!')
+  //       return;
+  //   }
+  //   res.end(data);
+  // });
+  let rs=fs.createReadStream("./files/bigfile.txt")
 
-myEmitter.on('createdWithIdandname',(id,name)=>{
-    console.log(`User ${name} Created with ${id} via emmiter !`)
-})
+  rs.on('data',(chunk)=>{
+    res.write("chunk###\n\n"+chunk)
+    res.end();
+  })
 
-myEmitter.emit('userCreated');
-
-myEmitter.emit('createdWithIdandname',101, "bereket")
+  rs.on('error',(err)=>{
+    res.status(500).end(err.message)
+  })
+  
+});
